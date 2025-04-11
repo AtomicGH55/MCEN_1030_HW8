e72a21f3e5486a0feae75d286cb9bde129e1ca87
AtomicGH55 <bradycalhoun@gmail.com>
2025-04-10 23:45:47 -0600

    getting started

---
+++ Star_Wars_Database.py
@@ -0,0 +1,16 @@
+def main():
+
+    force_users = {
+        "Luke Skywalker": "Jedi",
+        "Darth Vader": "Sith",
+        "Yoda": "Jedi",
+        "Obi-Wan Kenobi": "Jedi",
+        "Emperor Palpatine": "Sith",
+        "Darth Maul": "Sith",
+        "Mace Windu": "Jedi"
+    }
+
+    print(force_users[0])
+
+if __name__ == '__main__':
+    main()
